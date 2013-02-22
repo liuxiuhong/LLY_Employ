83fe55b5117a97aff226358f3f68f945e7b69838
liuxiuhong <liuxiuhong1988@126.com>
2013-02-22 11:25:10 +0800

    Initial Commit

---
+++ LLY_Employ/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  LLY_Employ
+//
+//  Created by Ibokan on 13-2-22.
+//  Copyright (c) 2013年 Ibokan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
